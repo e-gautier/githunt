44ea642d0012f067fbfd19a5f28b8fed4160c036
Etienne GAUTIER <etienne.gautier@outlook.com>
2018-10-07 13:37:59 +0200

    fix #3

--- src/components/App.js
+++ src/components/App.js
@@ -232,7 +232,7 @@ class App extends Component {
     );
 
     return (
-      <div className={this.state.darkMode ? 'dark' : 'light'}>
+      <div className={this.state.darkMode ? 'dark' : 'light'} id="theme-container">
         <div className="App container">
           <Helmet>
             <meta charSet="utf-8" />
