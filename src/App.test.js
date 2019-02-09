45d9f3917f14a320c0ec492b6ee92b7accbd4a52
Etienne GAUTIER <etienne.gautier@outlook.com>
2019-02-09 16:53:16 +0100

    format

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
-import { applyMiddleware, createStore } from "redux";
-import reducers from "./reducers";
-import thunkMiddleware from "redux-thunk";
-import { Provider } from "react-redux";
+import { applyMiddleware, createStore } from 'redux';
+import reducers from './reducers';
+import thunkMiddleware from 'redux-thunk';
+import { Provider } from 'react-redux';
 
 const store = createStore(reducers, applyMiddleware(thunkMiddleware));
 
@@ -13,7 +13,8 @@ it('renders App with the store without crashing', () => {
   ReactDOM.render(
     <Provider store={store}>
       <App />
-    </Provider>
-    , div);
+    </Provider>,
+    div
+  );
   ReactDOM.unmountComponentAtNode(div);
 });
