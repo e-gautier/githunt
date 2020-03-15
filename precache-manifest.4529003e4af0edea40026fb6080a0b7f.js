69bb4af1d8792aeedd94d9ab1fff122bff103029
Etienne GAUTIER <etienne.gautier@outlook.com>
2020-03-15 19:36:57 +0100

    Updates

--- precache-manifest.4529003e4af0edea40026fb6080a0b7f.js
+++ precache-manifest.4529003e4af0edea40026fb6080a0b7f.js
@@ -8,15 +8,15 @@ self.__precacheManifest = [
     "url": "./static/js/runtime~main.d653cc00.js"
   },
   {
-    "revision": "0996ba3c5a3441751bdc",
-    "url": "./static/js/main.8fca8aac.chunk.js"
+    "revision": "5058da2b17d116ee1cb3",
+    "url": "./static/js/main.257f69f3.chunk.js"
   },
   {
     "revision": "19a03a3ba58f1e1d1501",
     "url": "./static/js/2.914fdfbb.chunk.js"
   },
   {
-    "revision": "0996ba3c5a3441751bdc",
+    "revision": "5058da2b17d116ee1cb3",
     "url": "./static/css/main.90d843bb.chunk.css"
   },
   {
@@ -24,7 +24,7 @@ self.__precacheManifest = [
     "url": "./static/css/2.13d603c1.chunk.css"
   },
   {
-    "revision": "3f352f2683669b56e9066510b7023ad1",
+    "revision": "a231014f43301c8602d2a12081de19ff",
     "url": "./index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.7b1ec06bfe33eb8207adc05b201556e8.js"
+  "./precache-manifest.4529003e4af0edea40026fb6080a0b7f.js"
 );
 
 workbox.clientsClaim();
